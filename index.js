9512d48da3f9bbb4d199b633cb0777a8f943ca0e
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-07-18 18:03:40 -0400

    set up instances of classes

--- index.js
+++ index.js
@@ -1,128 +1,127 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const Employee = require("../lib/Employee");
-const Manager = require("../lib/Manager");
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
-
-
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+let answersArr = [];
 
 const engineerPrompts = () => {
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "What is your name?",
-      name: "name",
-    },
-    {
-      type: "input",
-      message: "What is your ID?",
-      name: "id",
-    },
-    {
-      type: "input",
-      message: "What is your email?",
-      name: "email",
-    },
-    {
-      type: "input",
-      message: "What is your Github username?",
-      name: "github",
-    }
-  ])
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your Github username?",
+        name: "github",
+      },
+    ])
 
-  .then((answers) => {
-  answersArr.push(answers);
-  console.log("engineer Q", answersArr);
-  return nextIdPrompt();
-})
-}
-
-
-
+    .then((answers) => {
+      const engineer = new Engineer(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.github
+      );
+      answersArr.push(engineer);
+      console.log("engineer Q", answersArr);
+      return nextIdPrompt();
+    });
+};
 
 const internPrompts = () => {
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "What is your name?",
-      name: "name",
-    },
-    {
-      type: "input",
-      message: "What is your ID?",
-      name: "id",
-    },
-    {
-      type: "input",
-      message: "What is your email?",
-      name: "email",
-    },
-    {
-      type: "input",
-      message: "What school do you attend?",
-      name: "school",
-    },
-  ])
-  .then((answers) => {
-    answersArr.push(answers);
-    console.log("intern Q", answersArr);
-    return nextIdPrompt();
-  
-  })
-}
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What school do you attend?",
+        name: "school",
+      },
+    ])
+    .then((answers) => {
+      const intern = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.school
+      );
+      answersArr.push(intern);
+      console.log("intern Q", answersArr);
+      return nextIdPrompt();
+    });
+};
 
+const managerPrompts = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your employee ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your employee email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your office number?",
+        name: "number",
+      },
+    ])
+    .then((answers) => {
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.number
+      );
+      answersArr.push(manager);
+      console.log("manager Q", answersArr);
+      return nextIdPrompt();
+    });
+};
 
-
-
-
-let answersArr =[];
-
-const managerPrompts =() =>{
-  inquirer.prompt([
-   
-    {
-      type: "input",
-      message: "What is your name?",
-      name: "name",
-    },
-    {
-      type: "input",
-      message: "What is your employee ID?",
-      name: "id",
-    },
-    {
-      type: "input",
-      message: "What is your employee email?",
-      name: "email",
-    },
-    {
-      type: "input",
-      message: "What is your office number?",
-      name: "number",
-    },
-
-  ])
-.then((answers) => {
-  const manager = new Manager(
-
-  )
-  answersArr.push(answers);
-  console.log("manager Q", answersArr);
-  return nextIdPrompt();
-})
-}
-
-
-
-
-
-
-
-
-const nextIdPrompt =() =>{
-  inquirer.prompt([
-    {
+const nextIdPrompt = () => {
+  inquirer
+    .prompt([
+      {
         type: "list",
         message: "What role would you like to create?",
         name: "role",
@@ -130,21 +129,21 @@ const nextIdPrompt =() =>{
       },
     ])
 
-   .then((answers) => {
-if (answers.role === "Engineer"){engineerPrompts();} 
-if (answers.role === "Intern"){internPrompts()};
-if (answers.role === "Finish Building My Team")
-{
-  console.log("finish Q", answersArr);
-  fs.writeFile("index.html", generateHTML(answers), (err) =>
-  err ? console.log(err) : console.log(`Success!`)
-   )
-}
-})
-}
-
-;
-
+    .then((answers) => {
+      if (answers.role === "Engineer") {
+        engineerPrompts();
+      }
+      if (answers.role === "Intern") {
+        internPrompts();
+      }
+      if (answers.role === "Finish Building My Team") {
+        console.log("finish Q", answersArr);
+        // fs.writeFile("index.html", generateHTML(answers), (err) =>
+        // err ? console.log(err) : console.log(`Success!`)
+        //  )
+      }
+    });
+};
 
 managerPrompts();
 module.exports = nextIdPrompt;
